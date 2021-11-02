3c0745429af2d5f8b548aad73958f7d2273ffa43
Khanh Nguyen <kn1600@nyu.edu>
2021-11-02 15:12:16 +0000

    Finish annotation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Pip + Logger = pogger
+
+### Setup
+
+Run `sh configure.sh` to set up traces directory and env variable
+
+#### Alternatively
+
+If you are in bash. You can run:
+- `mkdir traces`
+- `vi ~/.bash_profile`
+- `export TRACE_FILES=~/pogger/traces`
+- `soucre ~/.bash_profile`
--- apps/annotation/lib/annotation.ex
+++ apps/annotation/lib/annotation.ex
@@ -2,36 +2,32 @@ defmodule Annotation do
 
   import Emulation, only: [whoami: 0]
 
-  @send :send_msg
-  @receive :receive_msg
-
   @moduledoc """
   Documentation for `Annotation`.
   """
   
   
   def init(path_id) do
-    context_name = get_context()
+    context_name = whoami()
     try do
       Agent.stop(context_name)
     catch
       :exit, _ -> true
     end
-    
     Agent.start_link(fn -> Annotation.Path.init(path_id) end, name: context_name)
   end
 
   def terminate do
-    context_name = get_context()
+    context_name = whoami()
     try do
-      Agent.stop(@context_name)
+      Agent.stop(context_name)
     catch
       :exit, _ -> true
     end
   end
 
-  def get_path_name do
-    context_name = get_context()
+  def get_path_id do
+    context_name = whoami()
     Agent.get(context_name, fn path -> Map.get(path, :path_id) end)
   end
 
@@ -46,7 +42,7 @@ defmodule Annotation do
       timestamp,
       :tstart
     )
-    Agent.update(get_context(), fn path -> add_event(path, [task]) end)
+    Agent.update(whoami(), fn path -> add_event(path, [task]) end)
     output()
   end
 
@@ -57,7 +53,7 @@ defmodule Annotation do
       timestamp,
       :tend
     )
-    Agent.update(get_context(), fn path -> add_event(path, [task]) end)
+    Agent.update(whoami(), fn path -> add_event(path, [task]) end)
     output()
   end
 
@@ -65,9 +61,9 @@ defmodule Annotation do
     timestamp = get_timestamp(clock_value)
     notice = Annotation.Notice.init(
       name,
-      timestamp: timestamp
+      timestamp
     )
-    Agent.update(get_context(), fn path -> add_event(path, [notice]) end)
+    Agent.update(whoami(), fn path -> add_event(path, [notice]) end)
     output()
   end
 
@@ -79,7 +75,7 @@ defmodule Annotation do
       message_size,
       timestamp
     )
-    Agent.update(get_context(), fn path -> add_event(path, [send_msg]) end)
+    Agent.update(whoami(), fn path -> add_event(path, [send_msg]) end)
     output()
   end
 
@@ -91,28 +87,34 @@ defmodule Annotation do
       message_size,
       timestamp
     )
-    Agent.update(get_context(), fn path -> add_event(path, [received_msg]) end)
+    Agent.update(whoami(), fn path -> add_event(path, [received_msg]) end)
     output()
   end
 
   defp output() do
     file = get_file_path()
-    path = Agent.get(get_context(), fn path -> path end)
+    path = Agent.get(whoami(), fn path -> path end)
+    path_id = Map.get(path, :path_id)
     json_path = Jason.encode!(path)
     case file do
       nil -> IO.puts("#{inspect(json_path)}")
       fname ->
-        full_fname = "#{inspect(fname)}-#{inspect(whoami())}"
-        {:ok, file} = File.open(full_fname, [:write])
-        IO.binwrite(file, json_path)
-        File.close(file)
+        full_fname = "#{fname}/#{path_id}"
+        case File.touch!(full_fname) do
+          :ok ->
+            {:ok, file} = File.open(full_fname, [:write])
+            IO.binwrite(file, json_path)
+            File.close(file)
+        end
     end
   end
 
   defp read_from_file() do
     file = get_file_path()
+    path_id = get_path_id()
     if file != nil do
-      {:ok, path} = File.read(file)
+      full_fname = "#{file}/#{path_id}"
+      {:ok, path} = File.read(full_fname)
       decoded_path = Jason.decode!(path)
       IO.puts("#{inspect(decoded_path)}")
     end
@@ -122,14 +124,10 @@ defmodule Annotation do
     System.get_env("TRACE_FILES")
   end
 
-  def get_context do
-    :anno_context
-  end
-
   defp get_timestamp(clock_value) do
     Annotation.TimeStamp.init(
-      :a,
-      get_path_name(),
+      whoami(),
+      get_path_id(),
       clock_value
     )
   end
