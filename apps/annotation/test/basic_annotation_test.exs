54eb15093daaf1eb711c7c6c8f2968cf3047ccf2
Khanh Nguyen <kn1600@nyu.edu>
2021-11-02 15:12:16 +0000

    Finish annotation

--- apps/annotation/test/basic_annotation_test.exs
+++ apps/annotation/test/basic_annotation_test.exs
@@ -4,21 +4,44 @@ defmodule BasicAnnotationTest do
   import Kernel,
     except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
 
-  import Emulation, only: [spawn: 2, send: 2, whoami: 0, timer: 1]
-  import Annotation
+  import Emulation, only: [spawn: 2, send: 2]
 
   def basic_trace_with_task_annotation do
     Annotation.init("a")
     Annotation.annotate_start_task("1+1", 0)
-    x = 1 + 1
-    IO.puts("Output: #{inspect(x)}")
+    _x = 1 + 1
     Annotation.annotate_end_task("1+1", 0)
   end
 
-  test "Test basic trace with task annotation" do
+  def basic_trace_with_notice_annotation do
+    Annotation.init("b")
+    Annotation.annotate_notice("Starting process b", 0)
+  end
+
+  def basic_trace_with_send_and_receive_annotation(caller) do
+    Annotation.init("c")
+    msg = "Hello world!"
+    Annotation.annotate_send("c:send", byte_size(msg), 1)
+    send(self(), msg)
+    receive do
+      {_, _, received_m}
+        -> 
+        Annotation.annotate_receive("c:receive", byte_size(received_m), 2)
+        send(caller, :done)
+    end
+  end
+
+  test "Test trace with basic annotations" do
     Emulation.init()
     pid = self()
-    spawn(:a, basic_trace_with_task_annotation)
+    spawn(:a, fn -> basic_trace_with_task_annotation() end)
+    spawn(:b, fn -> basic_trace_with_notice_annotation() end)
+    spawn(:c, fn -> basic_trace_with_send_and_receive_annotation(pid) end)
+
+    receive do
+      :done -> true
+    end
+
   after
     Emulation.terminate()
   end
