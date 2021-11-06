3d6ccea8e4831ef684c273ff7646ac020ecc0959
Khanh Nguyen <kn1600@nyu.edu>
2021-11-06 03:30:02 +0000

    Make configuration file to be more robust

--- configure.sh
+++ configure.sh
@@ -1,2 +1,15 @@
+# /bin/bash
 mkdir traces 2> /dev/null
-set TRACE_FILES=~/pogger/traces
+mkdir expectations 2> /dev/null
+
+trace=$(echo "$TRACE_FILES")
+if [ -z "${trace}" ]; then
+  echo "export TRACE_FILES=\"~/pogger/traces\"" >> ~/.bashrc
+fi
+
+expectation=$(echo $EXPECTATION_FILES)
+if [ -z "${expectation}" ]; then
+  echo "export EXPECTATION_FILES=\"~/pogger/expectations\"" >> ~/.bashrc
+fi
+
+source ~/.bashrc
