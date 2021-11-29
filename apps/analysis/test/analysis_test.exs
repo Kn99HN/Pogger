0fc08abc96ead2470ae0dcd41e4ef65a77a4696b
Khanh Nguyen <kn1600@nyu.edu>
2021-11-29 19:32:12 +0000

    Add some basic testing. Not working just yet

--- apps/analysis/test/analysis_test.exs
+++ apps/analysis/test/analysis_test.exs
@@ -2,7 +2,46 @@ defmodule AnalysisTest do
   use ExUnit.Case
   doctest Analysis
 
-  test "greets the world" do
-    assert Analysis.hello() == :world
+  defp queue_server do 
+    queue_server([])
+  end
+
+  defp queue_server(queue) do
+    case receive do
+      {sender, {:enq, val} ->
+        queue_server([val] ++ queue)
+      {sender, {:deq}} ->
+        case queue do
+          [] -> raise "Queue is empty"
+          [head | tail] ->
+            send(sender, head)
+            queue_server(tail)
+        end
+    end
+  end
+
+  defp processA(server) do
+    Annotation.init("A")
+    msg_id = "a:enq"
+    Annotation.annotate_send(msg_id, byte_size(msg_id), 0)
+    send(server, {:end, 1})
+  end
+
+  defp processB(server) do
+    Annotation.init("B")
+    msg_id = "b:deq"
+    Annotation.annotate_send(msg_id, byte_size(msg_id), 0)
+    case receive do
+      {sender, val} ->
+        Annotation.annotate_receive("b:receive", val, 1)
+        Annotation.annotate_task(":incr", 0)
+        val = val + 1
+        send(sender, {:enq, val})
+        Annotation.annotate_send("b:send", val, 2)
+    end
+  end
+
+  test "basic traces with send and receive" do
+    Emulation.init()
   end
 end
