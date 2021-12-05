b89386f314ae5bea0da491ec546e2c49dbb19d0e
Joyce Lu <joycelu@Joyces-MBP.fios-router.home>
2021-12-05 12:36:11 -0500

    update the test expectation

--- apps/analysis/lib/analysis.ex
+++ apps/analysis/lib/analysis.ex
@@ -18,6 +18,7 @@ defmodule Analysis do
 
   defp queue_server(queue, time, log_path) do
     me = whoami()
+
     receive do
       {sender, {{:enq, val}, time_received}} ->
         time_current = update_vetor_clock(me, time)
@@ -59,6 +60,7 @@ defmodule Analysis do
 
   defp processB(server, time) do
     me = whoami()
+
     receive do
       {sender, {:start_deq, time_received}} ->
         log_path = "~/pogger/apps/analysis/lib/traces/test1"
@@ -92,11 +94,13 @@ defmodule Analysis do
     time = %{server: 0, a: 0, b: 0}
 
     case File.mkdir(Path.expand("~/pogger/apps/analysis/lib/traces/test1")) do
-          _ -> true
+      _ -> true
     end
+
     case File.mkdir_p(Path.expand("~/pogger/apps/analysis/lib/expectations-testfiles/test1")) do
-          _ -> true
+      _ -> true
     end
+
     server_log_path = "~/pogger/apps/analysis/lib/traces/test1"
 
     spawn(:server, fn -> queue_server(time, server_log_path) end)
@@ -108,10 +112,8 @@ defmodule Analysis do
         expectation_path = Path.expand("~/pogger/apps/analysis/lib/expectations-testfiles/test1")
         res = read_expectations_files(expectation_path)
         recognizers = res |> Enum.map(fn rec -> Map.get(rec, :expectation) end)
-        IO.puts("Expectation: #{inspect(recognizers)}")
         files = res |> Enum.map(fn rec -> Path.basename(Map.get(rec, :file)) end)
         trace_events = read_trace("test1")
-        IO.puts("Trace: #{inspect(trace_events)}")
         results = Enum.zip([Enum.reverse(files), check(recognizers, trace_events)])
         IO.puts("#{inspect(results)}")
     end
@@ -120,22 +122,22 @@ defmodule Analysis do
   end
 
   defp processC(time) do
-      log_path = "~/pogger/apps/analysis/lib/traces/test2"
-      Annotation.init("C", log_path)
-      me = whoami()
-      time = update_vetor_clock(me, time)
-      Annotation.annotate_send("c-enq", 0, time)
-      send(:server, {{:enq, 1}, time})
+    log_path = "~/pogger/apps/analysis/lib/traces/test2"
+    Annotation.init("C", log_path)
+    me = whoami()
+    time = update_vetor_clock(me, time)
+    Annotation.annotate_send("c-enq", 0, time)
+    send(:server, {{:enq, 1}, time})
   end
 
   defp processD(caller, time) do
-      log_path = "~/pogger/apps/analysis/lib/traces/test2"
-      Annotation.init("D", log_path)
-      me = whoami()
-      time = update_vetor_clock(me, time)
-      Annotation.annotate_send("d-enq", 0, time)
-      send(:server, {{:enq, 2}, time})
-      send(caller, true)
+    log_path = "~/pogger/apps/analysis/lib/traces/test2"
+    Annotation.init("D", log_path)
+    me = whoami()
+    time = update_vetor_clock(me, time)
+    Annotation.annotate_send("d-enq", 0, time)
+    send(:server, {{:enq, 2}, time})
+    send(caller, true)
   end
 
   def nonsequentail_send_and_receive do
@@ -144,25 +146,26 @@ defmodule Analysis do
     time = %{server: 0, c: 0, d: 0}
 
     case File.mkdir_p(Path.expand("~/pogger/apps/analysis/lib/traces/test2")) do
-          _ -> true
+      _ -> true
     end
+
     case File.mkdir_p(Path.expand("~/pogger/apps/analysis/lib/expectations-testfiles/test2")) do
-          _ -> true
+      _ -> true
     end
+
     server_log_path = "~/pogger/apps/analysis/lib/traces/test2"
 
     spawn(:server, fn -> queue_server(time, server_log_path) end)
     spawn(:c, fn -> processC(time) end)
     spawn(:d, fn -> processD(parent, time) end)
+
     receive do
       true ->
         expectation_path = Path.expand("~/pogger/apps/analysis/lib/expectations-testfiles/test2")
         res = read_expectations_files(expectation_path)
         recognizers = res |> Enum.map(fn rec -> Map.get(rec, :expectation) end)
-        IO.puts("Expectation: #{inspect(recognizers)}")
         files = res |> Enum.map(fn rec -> Path.basename(Map.get(rec, :file)) end)
         trace_events = read_trace("test2")
-        IO.puts("Trace: #{inspect(trace_events)}")
 
         results = Enum.zip([Enum.reverse(files), check(recognizers, trace_events)])
         IO.puts("#{inspect(results)}")
